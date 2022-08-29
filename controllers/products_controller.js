14b1b233b14db5d88a5e8fd31b42e2d9e5bd1123
aaronjohnston4 <aaron.johnston4@gmail.com>
2022-08-29 13:02:47 -0500

    show and index routes

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,12 +1,53 @@
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 
 
-// // MIDDLEWARE
-// router.use(express.json());
+// MIDDLEWARE
+router.use(express.json());
 
 
-// // MODEL IMPORT
-// const db = require("../models");
+// MODEL IMPORT
+const db = require("../models");
 
+
+// show route - http://localhost:XXXX/products/0
+// GET request for one product template
+router.get("/:productIndex", async (req, res) => {
+
+
+    try{
+  
+      const foundProduct = await db.Products.findById(req.params.productIndex)
+      // let product = products[req.params.productIndex];
+      res.render("show.ejs", { product: foundProduct, id: foundProduct._id });
+  
+  }catch(err){
+      // throw new Error(err)
+      console.log(err)
+      res.redirect('/404')
+  }
+  });
+  
+  // index - http://localhost:XXXX/products
+  // POST request for all products from products DB
+  router.get("/", async (req, res) => {
+  
+    try{
+  
+      const allProducts = await db.Products.find()
+      const context = { products: allProducts };
+      console.log(allProducts)
+      res.render("index.ejs", context);
+  
+  } catch(err){
+      // throw new Error(err)
+      console.log(err)
+      res.redirect('/404')
+  }  
+  });
+
+
+
+  
+  module.exports = router;
