1bd654fb247711397128692fb99a3dd8f8dab21e
DB-Butter <devbabs68@gmail.com>
2022-08-29 13:08:07 -0500

    changed room schema && rooms_model

--- models/rooms_model.js
+++ models/rooms_model.js
@@ -3,19 +3,19 @@ const rooms = [
         roomNumber: 101,
         price: 200,
         bedCount: 3,
-        availability: "available",
+        available: true,
     },
     {
         roomNumber: 102,
         price: 300,
         bedCount: 4,
-        availability: "available",
+        available: true,
     },
     {
         roomNumber: 103,
         price: 400,
         bedCount: 5,
-        availability: "available",
+        available: true,
     },
 ];
 
