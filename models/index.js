f8b74165560633dab956d2d6ebc1f2cd403098bf
Brian-Patterson <pbpatterson92@gmail.com>
2022-09-02 07:01:55 -0700

    setting up heroku

--- models/index.js
+++ models/index.js
@@ -3,3 +3,5 @@ module.exports = {
     Room: require('./Room'),
     Hotel: require('./Hotel'),
 }
+
+mongoose.connect( process.env.MONGODB_URI || "mongodb+srv://brian:brian@cluster0.ubtyygm.mongodb.net/?retryWrites=true&w=majority");
